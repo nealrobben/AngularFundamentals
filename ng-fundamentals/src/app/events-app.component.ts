dababe979c0099d97453d5e203abdb4aa8973358
nealrobben <neal.robben@cegeka.com>
2019-08-27 13:54:09 +0200

    Removed unused html/css classes

--- ng-fundamentals/src/app/events-app.component.ts
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root'
 })
 export class AppComponent {
   title = 'ng-fundamentals';
