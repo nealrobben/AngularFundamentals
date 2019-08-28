8b10f0139290001722b3f699c3dace9bd2fe750e
nealrobben <neal.robben@cegeka.com>
2019-08-28 09:22:43 +0200

    Template binding

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -1,21 +1,24 @@
 import { Component, Input, Output } from '@angular/core';
-import { EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location.address}}</span>
+            <span class="pad-left">{{event?.location.city}}, {{event?.location.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl">
+            Online URL: {{event?.onlineUrl}}
         </div>
     </div>`,
     styles:[
-        `.pad-left { margin-left: 10px;} 
+        `.thumbnail {min-height: 210px;}
+        .pad-left { margin-left: 10px;} 
         .well div {color: #bbb}`
     ]
 })
